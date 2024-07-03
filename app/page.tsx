d2029f04da9e7b5c7acd56f0d9fa1a687099bd37
Naomi Teruya <nahteruya@gmail.com>
2024-07-03 10:20:47 -0300

    feat: get products info from prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
-import Card from "./components/Card";
-import Header from "./components/Header";
+import Card from "./_components/Card";
+import Header from "./_components/Header";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+export default async function Home() {
+  const prisma = new PrismaClient();
+  const productList = await prisma.product.findMany();
   return (
     <main>
       <Header />
       <div className="relative top-14 mx-auto grid w-full max-w-6xl grid-cols-4 gap-4 px-4 pt-10">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {productList.map((product) => (
+          <Card key={product.id} product={product} />
+        ))}
       </div>
     </main>
   );
